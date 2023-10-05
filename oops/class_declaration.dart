4b208501c96067c659f2c5b6ae9b71f367ca5fd5
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-05 12:51:45 +0545

    class declaration in dart

---
+++ oops/class_declaration.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+class FlutterCourse{
+  String? flutterclass;
+  String? students;
+   String? Laptop;
+
+  void display(){
+    print("Des Vu Technologies organizes $flutterclass ");
+    print("There are a lot of learning $students");
+    print("Student brings their $Laptop");
+  }
+  
+  }
+
+  void main(){
+    FlutterCourse flucourse = FlutterCourse();
+    flucourse.students="college boys";
+    flucourse.Laptop = "their own laptop" ;
+    flucourse.flutterclass="Dart mobile banking application program.";
+    flucourse.display();
+  }
