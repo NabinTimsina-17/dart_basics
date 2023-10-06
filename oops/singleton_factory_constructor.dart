0d5cd736d63acdc75a9cf4ad17778d0cdddb51f4
NabinTimsina-17 <133372145+NabinTimsina-17@users.noreply.github.com>
2023-10-06 11:56:42 +0545

    singleton constructor in dart

---
+++ oops/singleton_factory_constructor.dart
@@ -0,0 +1,20 @@
+class SingletonExample{
+  static SingletonExample? _instance;
+  // ignore: unused_element
+  SingletonExample._();
+
+  factory SingletonExample.getInstance(){
+    if (_instance ==null){
+      _instance = SingletonExample._();
+    }
+      return _instance!;
+    
+  }
+}
+
+void main(){
+  SingletonExample obj1 = SingletonExample.getInstance();
+  SingletonExample obj2 = SingletonExample.getInstance();
+  print(identical(obj1, obj2));
+
+}
